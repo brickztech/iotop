2f985bd368952a52dd21d63a8a4c1fea06e743ea
Brigitta Arvay <arvay.brigitta22@gmail.com>
2020-04-07 09:47:12 +0200

    Changed config directive

--- rule-engine/rule-engine-components/src/main/resources/public/static/rulenode/sum-node-config.js
+++ rule-engine/rule-engine-components/src/main/resources/public/static/rulenode/sum-node-config.js
@@ -1,22 +1,9 @@
 angular.module('thingsboard.ruleChain.config.enrichment', [])
-.controller('Controller', ['$scope', function($scope) {
-  $scope.configuration = {
-    outputKey: 'ConsumptionSum'
-  };
-  $scope.tb = {
-    rulenode: {
-      "test-input": "Test input"
-  }
-  }
-}])
 .directive('tbEnrichmentNodeGetTelemetryAndSum', function() {
-  console.log("123");
   return {
     restrict: 'E',
+    require: "^ngModel",
     template: 
-    '<section layout=column> <md-input-container style=padding-bottom:15px>' +
-    '<input ng-model=configuration.outputKey placeholder="{{\'tb.rulenode.test-input\'}}" ng-required=true>' +
-    '</md-input-container>' +
-    '</section>'
+    "<section layout=column> <md-input-container style=padding-bottom:15px><input ng-model=configuration.outputKey ng-required=true aria-label={{tb.rulenode.testInput}}><label translate>{{tb.rulenode.testInput}}</label></md-input-container></section>"
   };
 });
