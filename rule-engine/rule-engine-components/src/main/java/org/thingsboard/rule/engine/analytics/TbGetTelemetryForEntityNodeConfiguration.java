bd9987d4ad21cdf8f4019fb288a1b2630c0d6dfb
Drávai Tamás <dravko1free@gmail.com>
2020-04-21 12:14:13 +0200

    Aggregate function added to node's configuration.

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/analytics/TbGetTelemetryForEntityNodeConfiguration.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/analytics/TbGetTelemetryForEntityNodeConfiguration.java
@@ -2,26 +2,27 @@ package org.thingsboard.rule.engine.analytics;
 
 import lombok.Data;
 import org.thingsboard.rule.engine.api.NodeConfiguration;
+import org.thingsboard.server.common.data.kv.Aggregation;
 
 @Data
 public class TbGetTelemetryForEntityNodeConfiguration implements NodeConfiguration<TbGetTelemetryForEntityNodeConfiguration> {
 
+    private String aggregateFunction;
     private String inputKey;
     private String outputKey;
-    private Aggregations aggregation;
-    public enum Aggregations {
-        AVG,
-        MAX,
-        MIN,
-        SUM
-    };
 
     @Override
     public TbGetTelemetryForEntityNodeConfiguration defaultConfiguration() {
         TbGetTelemetryForEntityNodeConfiguration configuration = new TbGetTelemetryForEntityNodeConfiguration();
-        configuration.setAggregation(Aggregations.SUM);
+        configuration.setAggregateFunction(Aggregation.SUM.toString());
         configuration.setInputKey("power");
         configuration.setOutputKey("PowerSum");
         return configuration;
+    }
+
+    public Aggregation getAggregation() {
+        if (aggregateFunction != null)
+            return Aggregation.valueOf(aggregateFunction);
+        return Aggregation.SUM;
     }
 }
