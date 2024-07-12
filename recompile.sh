0e18c088c9462914e61fa85b6b6e95d7b579d25d
Drávai Tamás <dravko1free@gmail.com>
2024-07-12 08:29:29 +0200

    Set JAVA_HOME before recompile and run

--- recompile.sh
+++ recompile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# nvm use v12.22.8
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/
+
 cd common/dao-api
 mvn clean install -DskipTests -Dlicense.skip=true
 
--- start-app.sh
+++ start-app.sh
@@ -4,4 +4,4 @@
 # export DATABASE_ENTITIES_TYPE=cassandra
 # export CASSANDRA_URL=localhost:9042
 
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dlogging.config=logback.xml ./application/target/thingsboard-3.3.1-boot.jar
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dlogging.config=logback.xml ./application/target/thingsboard-3.3.2-boot.jar
